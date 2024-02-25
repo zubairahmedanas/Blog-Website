cbb2eec6455fabba446d5dadebaf1ce345a3c506
zubair <zubairahmed.anas@bracits.com>
2024-02-25 17:56:52 +0600

    blog html file organising

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import *
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
