3e314ec2d7032808ec5a696ca9e45db0af0a17db
zubair <zubairahmed.anas@bracits.com>
2024-04-01 15:23:48 +0600

    Zubair | user and superuser created in managers file

--- user_profile/managers.py
+++ user_profile/managers.py
@@ -13,26 +13,27 @@ class CustomUserManager(BaseUserManager):
         user = self.model(
             username=username,
             email=email,
-            password=password
+            password=password,
                      ** extra_fields
         )
         user.set_password(password)
         user.save()
+        return user
 
-    def create_user(self, username, email, password, **extra_fields):
+    def create_superuser(self, username, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_super', True)
+        extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('is_staff will be true for superuser')
-        if extra_fields.get('is_super') is not True:
-            raise ValueError('is_super will be true for superuser')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('is_superuser will be true for superuser')
 
         user = self.create_user(
-            username = username,
+            username=username,
             email=email,
             password=password,
-            **extra_fields
+            **extra_fields,
         )
         return user
